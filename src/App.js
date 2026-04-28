dae82912271a15c73e42e780074770bf6a4a6a20
Aryan Gaikwad <vu4f2324012@pvppcoe.ac.in>
2026-04-29 00:34:39 +0530

    Added ecommerce UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const products = [
+    {
+      id: 1,
+      name: "Wireless Headphones",
+      price: "₹1999"
+    },
+    {
+      id: 2,
+      name: "Smart Watch",
+      price: "₹2999"
+    },
+    {
+      id: 3,
+      name: "Gaming Mouse",
+      price: "₹1499"
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>E-Commerce PWA Store</h1>
+      <p>Simple Product Listing Page</p>
+
+      <div className="product-grid">
+        {products.map((product) => (
+          <div className="card" key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.price}</p>
+            <button>Add to Cart</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
